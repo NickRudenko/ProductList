c5a8c6635b14af5db9df84b259419e38ba1be3b0
Mykola Rudenko <nickrudenkodev@gmail.com>
2022-10-14 22:18:49 +0100

    Initial Commit

---
+++ Reveri/ReveriApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReveriApp.swift
+//  Reveri
+//
+//  Created by Mykola Rudenko on 14.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReveriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
